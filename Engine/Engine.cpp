1902beadefe6ac8be7e9aeb7e7e92bbdbf292144
brett beloin <brettbeloin@gmail.com>
2026-06-30 10:57:10 -0600

    The start of the Game Engine

---
+++ Engine/Engine.cpp
@@ -0,0 +1,5 @@
+// #include "framework.h"
+#include <iostream>
+// #include <pch.h>
+
+void fnEngine() { std::cout << "welcome to the lib. \n"; }
---
+++ build/CMakeFiles/GameEngine.dir/src/Game.cpp.o.d
@@ -0,0 +1,225 @@
+CMakeFiles/GameEngine.dir/src/Game.cpp.o: \
+ /home/brett/GameEngine/src/Game.cpp /usr/include/stdc-predef.h \
+ /home/brett/GameEngine/src/../Engine/Engine.cpp \
+ /usr/include/c++/16/iostream /usr/include/c++/16/bits/requires_hosted.h \
+ /usr/include/c++/16/x86_64-redhat-linux/bits/c++config.h \
+ /usr/include/bits/wordsize.h \
+ /usr/include/c++/16/x86_64-redhat-linux/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/timesize.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/16/x86_64-redhat-linux/bits/cpu_defines.h \
+ /usr/include/c++/16/pstl/pstl_config.h /usr/include/c++/16/ostream \
+ /usr/include/c++/16/bits/ostream.h /usr/include/c++/16/ios \
+ /usr/include/c++/16/iosfwd /usr/include/c++/16/bits/stringfwd.h \
+ /usr/include/c++/16/bits/memoryfwd.h /usr/include/c++/16/bits/version.h \
+ /usr/include/c++/16/bits/postypes.h /usr/include/c++/16/cwchar \
+ /usr/include/wchar.h /usr/include/bits/libc-header-start.h \
+ /usr/include/bits/floatn.h /usr/include/bits/floatn-common.h \
+ /usr/lib/gcc/x86_64-redhat-linux/16/include/stddef.h \
+ /usr/lib/gcc/x86_64-redhat-linux/16/include/stdarg.h \
+ /usr/include/bits/wchar.h /usr/include/bits/types/wint_t.h \
+ /usr/include/bits/types/mbstate_t.h \
+ /usr/include/bits/types/__mbstate_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/locale_t.h \
+ /usr/include/bits/types/__locale_t.h /usr/include/c++/16/exception \
+ /usr/include/c++/16/bits/exception.h \
+ /usr/include/c++/16/bits/exception_ptr.h \
+ /usr/include/c++/16/bits/exception_defines.h \
+ /usr/include/c++/16/bits/cxxabi_init_exception.h \
+ /usr/include/c++/16/typeinfo /usr/include/c++/16/bits/hash_bytes.h \
+ /usr/include/c++/16/new /usr/include/c++/16/bits/new_except.h \
+ /usr/include/c++/16/bits/move.h /usr/include/c++/16/type_traits \
+ /usr/include/c++/16/bits/nested_exception.h \
+ /usr/include/c++/16/bits/char_traits.h /usr/include/c++/16/compare \
+ /usr/include/c++/16/concepts /usr/include/c++/16/bits/stl_construct.h \
+ /usr/include/c++/16/bits/stl_iterator_base_types.h \
+ /usr/include/c++/16/bits/iterator_concepts.h \
+ /usr/include/c++/16/bits/ptr_traits.h \
+ /usr/include/c++/16/bits/ranges_cmp.h \
+ /usr/include/c++/16/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/16/bits/concept_check.h \
+ /usr/include/c++/16/debug/assertions.h \
+ /usr/include/c++/16/bits/localefwd.h \
+ /usr/include/c++/16/x86_64-redhat-linux/bits/c++locale.h \
+ /usr/include/c++/16/clocale /usr/include/locale.h \
+ /usr/include/bits/locale.h /usr/include/c++/16/cctype \
+ /usr/include/ctype.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
+ /usr/include/bits/endian.h /usr/include/bits/endianness.h \
+ /usr/include/c++/16/bits/ios_base.h /usr/include/c++/16/ext/atomicity.h \
+ /usr/include/c++/16/x86_64-redhat-linux/bits/gthr.h \
+ /usr/include/c++/16/x86_64-redhat-linux/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h \
+ /usr/include/bits/types/time_t.h \
+ /usr/include/bits/types/struct_timespec.h /usr/include/bits/sched.h \
+ /usr/include/linux/sched/types.h /usr/include/linux/types.h \
+ /usr/include/asm/types.h /usr/include/asm-generic/types.h \
+ /usr/include/asm-generic/int-ll64.h /usr/include/asm/bitsperlong.h \
+ /usr/include/asm-generic/bitsperlong.h /usr/include/linux/posix_types.h \
+ /usr/include/linux/stddef.h /usr/include/asm/posix_types.h \
+ /usr/include/asm/posix_types_64.h /usr/include/asm-generic/posix_types.h \
+ /usr/include/bits/types/struct_sched_param.h /usr/include/bits/cpu-set.h \
+ /usr/include/time.h /usr/include/bits/time.h /usr/include/bits/timex.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/struct_tm.h \
+ /usr/include/bits/types/clockid_t.h /usr/include/bits/types/timer_t.h \
+ /usr/include/bits/types/struct_itimerspec.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h \
+ /usr/include/bits/atomic_wide_counter.h /usr/include/bits/struct_mutex.h \
+ /usr/include/bits/struct_rwlock.h /usr/include/bits/setjmp.h \
+ /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct___jmp_buf_tag.h \
+ /usr/include/bits/pthread_stack_min-dynamic.h \
+ /usr/include/c++/16/x86_64-redhat-linux/bits/atomic_word.h \
+ /usr/include/sys/single_threaded.h \
+ /usr/include/c++/16/bits/locale_classes.h \
+ /usr/include/c++/16/bits/functexcept.h /usr/include/c++/16/string \
+ /usr/include/c++/16/bits/allocator.h \
+ /usr/include/c++/16/x86_64-redhat-linux/bits/c++allocator.h \
+ /usr/include/c++/16/bits/new_allocator.h \
+ /usr/include/c++/16/bits/new_throw.h \
+ /usr/include/c++/16/bits/cpp_type_traits.h \
+ /usr/include/c++/16/bits/ostream_insert.h \
+ /usr/include/c++/16/bits/cxxabi_forced.h \
+ /usr/include/c++/16/bits/stl_iterator.h \
+ /usr/include/c++/16/ext/type_traits.h /usr/include/c++/16/bits/utility.h \
+ /usr/include/c++/16/bits/stl_function.h \
+ /usr/include/c++/16/backward/binders.h \
+ /usr/include/c++/16/ext/numeric_traits.h \
+ /usr/include/c++/16/bits/stdexcept_throw.h \
+ /usr/include/c++/16/bits/stdexcept_throwfwd.h \
+ /usr/include/c++/16/bits/stl_algobase.h \
+ /usr/include/c++/16/bits/stl_pair.h /usr/include/c++/16/debug/debug.h \
+ /usr/include/c++/16/bits/predefined_ops.h /usr/include/c++/16/bit \
+ /usr/include/c++/16/bits/range_access.h \
+ /usr/include/c++/16/initializer_list /usr/include/c++/16/bits/erase_if.h \
+ /usr/include/c++/16/bits/basic_string.h \
+ /usr/include/c++/16/ext/alloc_traits.h \
+ /usr/include/c++/16/bits/alloc_traits.h /usr/include/c++/16/string_view \
+ /usr/include/c++/16/bits/functional_hash.h \
+ /usr/include/c++/16/bits/ranges_base.h \
+ /usr/include/c++/16/bits/max_size_type.h /usr/include/c++/16/numbers \
+ /usr/include/c++/16/limits /usr/include/c++/16/bits/string_view.tcc \
+ /usr/include/c++/16/bits/ranges_algobase.h \
+ /usr/include/c++/16/bits/invoke.h /usr/include/c++/16/bits/ranges_util.h \
+ /usr/include/c++/16/ext/string_conversions.h /usr/include/c++/16/cstdlib \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/sys/types.h \
+ /usr/include/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/uintn-identity.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/types/sigset_t.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-float.h /usr/include/c++/16/bits/std_abs.h \
+ /usr/include/c++/16/cstdio /usr/include/stdio.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__fpos64_t.h \
+ /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/types/cookie_io_functions_t.h \
+ /usr/include/bits/stdio_lim.h /usr/include/c++/16/cerrno \
+ /usr/include/errno.h /usr/include/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/bits/types/error_t.h /usr/include/c++/16/bits/charconv.h \
+ /usr/include/c++/16/bits/basic_string.tcc \
+ /usr/include/c++/16/bits/memory_resource.h /usr/include/c++/16/cstddef \
+ /usr/include/c++/16/bits/uses_allocator.h \
+ /usr/include/c++/16/bits/uses_allocator_args.h /usr/include/c++/16/tuple \
+ /usr/include/c++/16/bits/locale_classes.tcc \
+ /usr/include/c++/16/system_error \
+ /usr/include/c++/16/x86_64-redhat-linux/bits/error_constants.h \
+ /usr/include/c++/16/stdexcept \
+ /usr/include/c++/16/bits/stdexcept_except.h \
+ /usr/include/c++/16/streambuf /usr/include/c++/16/bits/streambuf.tcc \
+ /usr/include/c++/16/bits/basic_ios.h \
+ /usr/include/c++/16/bits/locale_facets.h /usr/include/c++/16/cwctype \
+ /usr/include/wctype.h /usr/include/bits/wctype-wchar.h \
+ /usr/include/c++/16/x86_64-redhat-linux/bits/ctype_base.h \
+ /usr/include/c++/16/bits/streambuf_iterator.h \
+ /usr/include/c++/16/x86_64-redhat-linux/bits/ctype_inline.h \
+ /usr/include/c++/16/bits/locale_facets.tcc \
+ /usr/include/c++/16/bits/basic_ios.tcc /usr/include/c++/16/format \
+ /usr/include/c++/16/array /usr/include/c++/16/charconv \
+ /usr/include/c++/16/locale \
+ /usr/include/c++/16/bits/locale_facets_nonio.h /usr/include/c++/16/ctime \
+ /usr/include/c++/16/x86_64-redhat-linux/bits/time_members.h \
+ /usr/include/c++/16/x86_64-redhat-linux/bits/messages_members.h \
+ /usr/include/libintl.h /usr/include/c++/16/bits/codecvt.h \
+ /usr/include/c++/16/bits/locale_facets_nonio.tcc \
+ /usr/include/c++/16/bits/locale_conv.h /usr/include/c++/16/optional \
+ /usr/include/c++/16/bits/enable_special_members.h \
+ /usr/include/c++/16/span /usr/include/c++/16/bits/monostate.h \
+ /usr/include/c++/16/bits/formatfwd.h /usr/include/c++/16/bits/unicode.h \
+ /usr/include/c++/16/cstdint \
+ /usr/lib/gcc/x86_64-redhat-linux/16/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/bits/stdint-least.h /usr/include/c++/16/bits/stl_algo.h \
+ /usr/include/c++/16/bits/algorithmfwd.h \
+ /usr/include/c++/16/bits/stl_heap.h \
+ /usr/include/c++/16/bits/uniform_int_dist.h \
+ /usr/include/c++/16/bits/stl_tempbuf.h \
+ /usr/include/c++/16/bits/unicode-data.h \
+ /usr/include/c++/16/bits/ostream.tcc /usr/include/c++/16/istream \
+ /usr/include/c++/16/bits/istream.tcc \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_stdinc.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_platform_defines.h \
+ /usr/include/string.h /usr/include/strings.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_begin_code.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_close_code.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_assert.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_asyncio.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_atomic.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_audio.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_endian.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_error.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_mutex.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_thread.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_properties.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_iostream.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_bits.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_blendmode.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_camera.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_pixels.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_surface.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_rect.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_clipboard.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_cpuinfo.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_dialog.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_video.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_dlopennote.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_events.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_gamepad.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_guid.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_joystick.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_power.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_sensor.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_keyboard.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_keycode.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_scancode.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_mouse.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_pen.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_touch.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_filesystem.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_gpu.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_haptic.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_hidapi.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_hints.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_init.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_loadso.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_locale.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_log.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_messagebox.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_metal.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_misc.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_platform.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_process.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_render.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_storage.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_system.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_time.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_timer.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_tray.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_version.h \
+ /home/brett/GameEngine/external/SDL3/include/SDL3/SDL_oldnames.h
