e96d45ffb333bf4404b1799036a81f5fb728ab94
brett beloin <brettbeloin@gmail.com>
2026-06-30 22:02:43 -0600

    made minor tweaks to variable names

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,13 +1,17 @@
-#include "../Engine/Engine.cpp"
+// #include "../Engine/Engine.cpp"
 #include <SDL3/SDL.h>
 #include <iostream>
 
-#define TEXT "hello"
-
 int main(int argc, char *argv[]) {
   SDL_Init(SDL_INIT_VIDEO);
+  int window_width, window_height;
+  window_width = window_height = 500;
 
-  SDL_Window *window = SDL_CreateWindow("SDL3 Project", 1280, 1024, 0);
+  float rect_width, rect_height;
+  rect_width = rect_height = 50.0f;
+
+  SDL_Window *window =
+      SDL_CreateWindow("Game Engine", window_width, window_height, 0);
   if (window == nullptr) {
     std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
     SDL_Quit();
@@ -26,7 +30,10 @@ int main(int argc, char *argv[]) {
   bool quit = false;
 
   // Define a rectangle
-  SDL_FRect greenSquare{270, 190, 200, 200};
+  SDL_FRect greenSquare{
+      (static_cast<float>(window_width) / 2) - (rect_width / 2),
+      (static_cast<float>(window_height) / 2) - (rect_height / 2), rect_width,
+      rect_height};
 
   while (!quit) {
     while (SDL_PollEvent(&e)) {
@@ -42,6 +49,9 @@ int main(int argc, char *argv[]) {
     SDL_SetRenderDrawColor(renderer, 0, 255, 0,
                            255); // Set render draw color to green
     SDL_RenderFillRect(renderer, &greenSquare); // Render the rectangle
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Set text Color to red
+    SDL_RenderDebugText(renderer, 10, 10, "Hello world"); // Render the text
 
     SDL_RenderPresent(renderer); // Render the screen
   }
