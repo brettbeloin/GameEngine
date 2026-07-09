09d6ac3473668c2f226b20ce1619b42cebb208c6
brett beloin <brettbeloin@gmail.com>
2026-07-09 17:28:03 -0600

    add math lib

---
+++ headers/MathUtils.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <algorithm>
+namespace Engine::Math {
+    template <typename T> T Wrap(T min, T max, T value) {
+        if (value > max) {
+            value = min;
+        }
+        if (value < min) {
+            value = max;
+        }
+
+        return value;
+    }
+
+    template <typename T> T Min(T a, T b) {
+        return (a < b) ? a : b;
+    }
+
+    template <typename T> T Max(T a, T b) {
+        return (a > b) ? a : b;
+    }
+
+    template <typename T> T Clamp(T min, T max, T value) {
+        if (value > max) {
+            value = max;
+        }
+        if (value < min) {
+            value = min;
+        }
+
+        return value;
+    }
+
+    float Pi = 3.14159265358979323846264338327950288419716939937510f;
+    float TwoPi = Pi * 2;
+    float HalfPi = Pi / 2;
+
+} // namespace Engine::Math
