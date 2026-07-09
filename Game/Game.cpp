c9db138a797b03aaceceeb568e14d17dbe640510
brett beloin <brettbeloin@gmail.com>
2026-07-08 22:15:16 -0600

    Paint assignment

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -7,14 +7,15 @@
 
 int main(int argc, char *argv[]) {
     Engine::Renderer             renderer;
-    constexpr Engine::Window     window = {"Game Engine", 500, 500}; // Set the window width and height
+    constexpr Engine::Window     window = {"Game Engine", 1'000, 1'000}; // Set the window width and height
 
     Engine::Time                 time;
 
     Engine::Input                input;
 
     std::vector<Engine::Vector2> points;
-    Engine::Vector2 pos{(static_cast<float>(window.window_width) / 2), (static_cast<float>(window.window_height) / 2)};
+    // Engine::Vector2 pos{(static_cast<float>(window.window_width) / 2), (static_cast<float>(window.window_height) /
+    // 2)};
 
     if (const bool initSuccess = renderer.Initialize(window); !initSuccess) {
         std::cerr << "Failed to initialize the renderer." << std::endl;
@@ -44,28 +45,24 @@ int main(int argc, char *argv[]) {
         input.Update();
         time.Tick();
 
-        if (input.GetButtonDown(Engine::Input::MouseButton::LEFT)) {
-            points.push_back(input.GetMousePosition());
-        }
+        // float           speed = 100.f;
+        // Engine::Vector2 volocity{0.0f, 0.0f};
 
-        float           speed = 100.f;
-        Engine::Vector2 volocity{0.0f, 0.0f};
+        // if (input.GetKeyDown(SDL_SCANCODE_A)) {
+        //     volocity.x += 1 - speed;
+        // }
+        //
+        // if (input.GetKeyDown(SDL_SCANCODE_D)) {
+        //     volocity.x += 1 + speed;
+        // }
+        // if (input.GetKeyDown(SDL_SCANCODE_W)) {
+        //     volocity.y += 1 - speed;
+        // }
+        // if (input.GetKeyDown(SDL_SCANCODE_S)) {
+        //     volocity.y += 1 + speed;
+        // }
 
-        if (input.GetKeyDown(SDL_SCANCODE_A)) {
-            volocity.x += 1 - speed;
-        }
-
-        if (input.GetKeyDown(SDL_SCANCODE_D)) {
-            volocity.x += 1 + speed;
-        }
-        if (input.GetKeyDown(SDL_SCANCODE_W)) {
-            volocity.y += 1 - speed;
-        }
-        if (input.GetKeyDown(SDL_SCANCODE_S)) {
-            volocity.y += 1 + speed;
-        }
-
-        pos += (volocity * time.GetDeltaTime());
+        // pos += (volocity * time.GetDeltaTime());
 
         // RENDERER
 
@@ -76,13 +73,38 @@ int main(int argc, char *argv[]) {
 
         renderer.Clear();                        // Clear the screen
 
-        for (int i = 0; i < points.size(); i++) {
-            renderer.SetColor(Engine::RandomFloat(256), Engine::RandomFloat(256), Engine::RandomFloat(256), 255);
-            renderer.DrawFillRect(points[i].x, points[i].y, 10, 10);
+        // step one
+        if (input.GetButtonPressed(Engine::Input::MouseButton::LEFT)) {
+            // TODO: Add mouse position to std::vector.
+            points.push_back(input.GetMousePosition());
         }
 
-        renderer.SetColor(255.0f, 0.0f, 0.0f, 255);
-        renderer.DrawFillRect(pos.x - 20, pos.y - 20, 40, 40);
+        // step two
+        Engine::Vector2 position = input.GetMousePosition();
+        if (input.GetButtonDown(Engine::Input::MouseButton::LEFT)) {
+            if (points.empty()) {
+                points.push_back(input.GetMousePosition());
+            }
+
+            if ((position - points.back()).length() > 10) {
+                points.push_back(input.GetMousePosition());
+            }
+        }
+
+        for (int i = 0; i < static_cast<int>(points.size()) - 1; i++) {
+            // TODO: Set color or random color.
+            // TODO: Call Renderer DrawLine with the std::vector [i] and [i + 1]
+            renderer.SetColor(Engine::RandomFloat(256), Engine::RandomFloat(256), Engine::RandomFloat(256), 255);
+            renderer.DrawLine(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+        }
+
+        // for (int i = 0; i < static_cast<int>(points.size()) - 1; i++) {
+        //     renderer.SetColor(Engine::RandomFloat(256), Engine::RandomFloat(256), Engine::RandomFloat(256), 255);
+        //     renderer.DrawLine(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+        // }
+
+        // renderer.SetColor(255.0f, 0.0f, 0.0f, 255);
+        // renderer.DrawFillRect(pos.x - 20, pos.y - 20, 40, 40);
 
         renderer.Present(); // Present the rendered content to the
                             // screen
